1aa3b02620568e96163178e8309e44591e0ffcbd
Daniel Cárdenas <cardenasluengo2022@gmail.com>
2024-07-03 10:07:18 -0400

    uodate voyager controller y public assets

---
+++ proyectoMuni-laravel/app/Http/Controllers/Voyager/Controller/VoyagerBaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Voyager\Controller;
+
+use TCG\Voyager\Http\Controllers\VoyagerBaseController as BaseVoyagerBaseController;
+
+class VoyagerBaseController extends BaseVoyagerBaseController
+{
+    //
+}
---
+++ proyectoMuni-laravel/app/Http/Controllers/Voyager/Controller/VoyagerRoleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Voyager\Controller;
+
+use TCG\Voyager\Http\Controllers\VoyagerRoleController as BaseVoyagerRoleController;
+
+class VoyagerRoleController extends BaseVoyagerRoleController
+{
+    //
+}
